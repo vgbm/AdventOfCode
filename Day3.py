35ac41d06be270218c06f71816c3e19975862c90
James <james@localhost.localdomain>
2015-12-04 16:25:07 -0500

    Finished Day 3 p. 2

--- Day3.py
+++ Day3.py
@@ -16,25 +16,38 @@ def readFileIntoStr(fileName):
 	return content[0].strip() 
 
 
+#single santa visiting houses based on the instruction list
+def PartOneSolution(instructions):
+	housesSet = getHousesVisited(instructions)
+	return len(housesSet)
+
+
+def PartTwoSolution(instructions):
+	
+	#santa begins at first instruction an goes every other
+	santaHouses = getHousesVisited(instructions[::2])
+	
+	#roboSanta begins at the second instruction and goes every other
+	roboHouses = getHousesVisited(instructions[1::2])
+	
+	#unions the sets to weed out repeats and then pulls the length to get overall num of houses
+	return len( set().union(santaHouses,roboHouses) )
+
+
 #uses a set to count only unique houses; goes through the
 #instruction list and follows each instruction one-by-one,
 #changing Santa's coordinate based on the instruction
-def PartOneSolution(instructions):
+def getHousesVisited(instructions):
 	coordinate = Pair(0,0)
 	houses = {coordinate}
 	
 	for instruction in instructions:
 		coordinate = followInstruction(coordinate,instruction)
-		print "{} for {}".format(coordinate,instruction)
 		houses.add(coordinate)
 	
-	print houses
-	return len(houses)
-
-
-def PartTwoSolution():
-	pass
-
+	return houses
+      
+      
 #changes the x or y pair based on the instruction
 def followInstruction(coordinate, instruction):
 	if instruction == '>':
@@ -53,4 +66,5 @@ if len(sys.argv) != 2:
 	raise Exception("Missing or too many command line arguments; needs a file argument!")
 
 instructions = readFileIntoStr(sys.argv[1])
-print PartOneSolution(instructions)
+print "Santa alone visits {} houses".format(PartOneSolution(instructions))
+print "Santa and RoboSanta visit {} houses".format(PartTwoSolution(instructions))
